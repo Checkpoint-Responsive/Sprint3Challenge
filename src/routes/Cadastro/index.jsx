23fa8baee60c354b6e66df9d494798685876a212
Pedro Gomes <pedro.gomesf2004@gmail.com>
2023-09-14 00:41:24 -0300

    criado todos css's  e feito html de 3 paginas.

--- src/routes/Cadastro/index.jsx
+++ src/routes/Cadastro/index.jsx
@@ -1,7 +1,40 @@
+import camera from '../../img/camera.png'
+import './cadastro.css'
 export default function Cadastro(){
-    return(
-        <main>
-            <h1>Cadastro</h1>
-        </main>
+
+
+    return (
+<>
+<header>
+    <h1>Cadastro da bike</h1></header>
+
+<main>
+
+    <section className="cadastro">
+            <div className="box">
+                <p>Tire uma foto da bicicleta</p>
+                <button className="btnCamera"><img src={camera} alt="" /></button>
+            </div>
+            <div className="box">
+                <p>Tire uma foto do guidão</p>
+                <button className="btnCamera"><img src={camera} alt="" /></button>
+                </div>
+            <div className="box">
+                <p>Tire uma foto do banco</p>
+                <button className="btnCamera"><img src={camera} alt="" /></button>
+                </div>
+            <div className="box">
+                <p>Tire uma foto das rodas</p>
+                <button className="btnCamera"><img src={camera} alt="" /></button>
+            </div>
+            <div className="box">
+            <button type = "submit" className="btnEnviar">Enviar</button>
+            </div>
+    </section>
+</main>
+<footer className="footer">
+      <img src={sair} alt="Sair"/>
+  </footer>
+</>
     )
 }
---
+++ src/routes/CadastroPessoa/index.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import './cadastro_pessoa.css';
+
+export default function PaginaCadastro() {
+    return (
+        <>
+            <header>
+                <h1>Cadastro Proprietário</h1>
+            </header>
+
+            <main>
+                <section className="Cadastro_Prop">
+
+                    <h2>Cadastre-se</h2>
+
+                    <div className="form-group">
+                        <label htmlFor="Name">Nome Completo:</label>
+                        <input type="text" id="Name" className="custom-input" placeholder="Digite seu Nome Completo" />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="cpf">CPF:</label>
+                        <input type="text" id="cpf" className="custom-input" placeholder="Digite seu CPF" />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="rg">RG:</label>
+                        <input type="text" id="rg" className="custom-input" placeholder="Digite seu RG" />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="endereco">Endereço:</label>
+                        <input type="text" id="endereco" className="custom-input" placeholder="Digite seu Endereço" />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" id="email" className="custom-input" placeholder="Digite seu Email" />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="senha">Senha:</label>
+                        <input type="password" id="senha" className="custom-input" placeholder="Digite sua Senha" />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="conf_email">Confirmar Email:</label>
+                        <input type="email" id="conf_email" className="custom-input" placeholder="Confirme seu Email" />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="conf_senha">Confirmar Senha:</label>
+                        <input type="password" id="conf_senha" className="custom-input" placeholder="Confirme sua Senha" />
+                    </div>
+
+                    <img src="./img/pefil.png" alt="Perfil" />
+
+                    <div className="box">
+                    <p>Tire ua foto</p>
+                    <button className="btnCamera"><img src={camera} alt="" /></button>
+                    </div>
+                    
+                    <img src="./img/sair.png" alt="Sair" />
+
+                </section>
+            </main>
+            
+            <footer className="footer">
+                <img src={sair} alt="Sair"/>
+            </footer>
+
+        </>
+    )
+}
