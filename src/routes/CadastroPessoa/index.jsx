7f5c10c357180a4ab28831fd6c2eb0602415fbf1
DuduViolante <126472870+DuduViolante@users.noreply.github.com>
2023-09-15 00:52:08 -0300

    home, login, cadastro, perfil

--- src/routes/CadastroPessoa/index.jsx
+++ src/routes/CadastroPessoa/index.jsx
@@ -6,70 +6,58 @@ import sair from '../img/sair.png'
 
 export default function CadastroPessoa() {
     return (
-        <>
+        <main >
             <header>
                 <h1>Cadastro Proprietário</h1>
+                    <h2>Cadastre-se</h2>
             </header>
 
             <main>
-                <section className="Cadastro_Prop">
+                <section className='container'>
 
-                    <h2>Cadastre-se</h2>
 
-                    <div className="form-group">
-                        <label htmlFor="Name">Nome Completo:</label>
-                        <input type="text" id="Name" className="custom-input" placeholder="Digite seu Nome Completo" />
+                    <div className="divInput" >
+                        <input type="text" id="Name" className="input"  placeholder="Digite seu Nome Completo" />
                     </div>
 
-                    <div className="form-group">
-                        <label htmlFor="cpf">CPF:</label>
-                        <input type="text" id="cpf" className="custom-input" placeholder="Digite seu CPF" />
+                    <div className="divInput" >
+                        <input type="text" id="cpf" className="input"  placeholder="Digite seu CPF" />
                     </div>
 
-                    <div className="form-group">
-                        <label htmlFor="rg">RG:</label>
-                        <input type="text" id="rg" className="custom-input" placeholder="Digite seu RG" />
+                    <div className="divInput" >
+                        <input type="text" id="rg" className="input"  placeholder="Digite seu RG" />
                     </div>
 
-                    <div className="form-group">
-                        <label htmlFor="endereco">Endereço:</label>
-                        <input type="text" id="endereco" className="custom-input" placeholder="Digite seu Endereço" />
+                    <div className="divInput" >
+                        <input type="text" id="endereco" className="input"  placeholder="Digite seu Endereço" />
                     </div>
 
-                    <div className="form-group">
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" className="custom-input" placeholder="Digite seu Email" />
+                    <div className="divInput" >
+                        <input type="email" id="email" className="input"  placeholder="Digite seu Email" />
                     </div>
 
-                    <div className="form-group">
-                        <label htmlFor="senha">Senha:</label>
-                        <input type="password" id="senha" className="custom-input" placeholder="Digite sua Senha" />
+                    <div className="divInput" >
+                        <input type="password" id="senha" className="input"  placeholder="Digite sua Senha" />
                     </div>
 
-                    <div className="form-group">
-                        <label htmlFor="conf_email">Confirmar Email:</label>
-                        <input type="email" id="conf_email" className="custom-input" placeholder="Confirme seu Email" />
+                    <div className="divInput" >
+                        <input type="email" id="conf_email" className="input"  placeholder="Confirme seu Email" />
                     </div>
 
-                    <div className="form-group">
-                        <label htmlFor="conf_senha">Confirmar Senha:</label>
-                        <input type="password" id="conf_senha" className="custom-input" placeholder="Confirme sua Senha" />
+                    <div className="divInput" >
+                        <input type="password" id="conf_senha" className="input"  placeholder="Confirme sua Senha" />
                     </div>
 
-                    <img src={perfil} alt="Perfil" />
 
-                    <div className="box">
-                    <p>Tire sua foto</p>
-                    <button className="btnCamera"><img src={camera} alt="" /></button>
-                    </div>
+                    
                     
                 </section>
             </main>
             
             <footer className="footer">
-                <Link to="/Home"> <img src={sair} alt="Sair"/> </Link>
+                <Link to="/Home" className='linkFooter'> <img src={sair} alt="Sair"/> Sair</Link>
             </footer>
 
-        </>
+        </main>
     )
 }
